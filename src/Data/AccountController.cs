c3cf38091cade84d790db1832f397b82ebb55cc9
Passive <passivemodding@gmail.com>
2019-12-10 21:55:42 +1100

    Initial code commit

---
+++ src/Data/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.DataProtection;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace BlazorLoginDiscord.Data
+{
+    [Route("[controller]/[action]")]
+    public class AccountController : ControllerBase
+    {
+        public IDataProtectionProvider Provider { get; }
+
+        public AccountController(IDataProtectionProvider provider)
+        {
+            Provider = provider;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl = "/")
+        {
+            return Challenge(new AuthenticationProperties { RedirectUri = returnUrl }, "Discord");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> LogOut(string returnUrl = "/")
+        {
+            //This removes the cookie assigned to the user login.
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return LocalRedirect(returnUrl);
+        }
+    }
+}
---
+++ src/Discord.OAuth2/DiscordExtensions.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using Microsoft.AspNetCore.Authentication;
+using Discord.OAuth2;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class DiscordAuthenticationOptionsExtensions
+    {
+        public static AuthenticationBuilder AddDiscord(this AuthenticationBuilder builder)
+            => builder.AddDiscord(DiscordDefaults.AuthenticationScheme, _ => { });
+
+        public static AuthenticationBuilder AddDiscord(this AuthenticationBuilder builder, Action<DiscordOptions> configureOptions)
+            => builder.AddDiscord(DiscordDefaults.AuthenticationScheme, configureOptions);
+
+        public static AuthenticationBuilder AddDiscord(this AuthenticationBuilder builder, string authenticationScheme, Action<DiscordOptions> configureOptions)
+            => builder.AddDiscord(authenticationScheme, DiscordDefaults.DisplayName, configureOptions);
+
+        public static AuthenticationBuilder AddDiscord(this AuthenticationBuilder builder, string authenticationScheme, string displayName, Action<DiscordOptions> configureOptions)
+            => builder.AddOAuth<DiscordOptions, DiscordHandler>(authenticationScheme, displayName, configureOptions);
+    }
+}
